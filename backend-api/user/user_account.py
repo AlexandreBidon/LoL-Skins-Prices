796ab00396d0f56185fd960bd1fbda2033c7c08b
Alexandre <alexandre.bidon.44@gmail.com>
2023-02-14 16:12:46 +0100

    email check

--- backend-api/user/user_account.py
+++ backend-api/user/user_account.py
@@ -5,6 +5,7 @@ from database.database import DataBase
 import logging
 import os
 import re
+from email_validator import validate_email, EmailNotValidError
 
 logger = logging.getLogger('python_logs')
 
@@ -15,12 +16,17 @@ class UserAccount():
         ,name :str
         ,email : str
         ,skin_list : list):
-        if self.__check_email(email):
+        try:
+            # validate and get info
+            v = validate_email(email)
+            # replace with normalized form
+            email = v["email"] 
             self.name = name
             self.email = email
             self.skin_list = skin_list
-        else:
-            raise(SyntaxError("Email not valid"))
+        except EmailNotValidError as e:
+            # email is not valid, exception message is human-readable
+            raise(e)
 
     def __check_email(self, email):   
         regex = """(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""" 
